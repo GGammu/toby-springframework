571301b97c80576a41ef92d9aba90a838cdc8596
Young Hwang <ykcul02@gmail.com>
2021-04-29 23:28:35 +0900

    여러 DAO가 있을 시 Factory 클래스 수정
    여러 DAO 추가에 따른 connection 객체 생성 메소드 추가

--- src/main/java/io/ggammu/study/tobyspringframework/user/domain/DaoFactory.java
+++ src/main/java/io/ggammu/study/tobyspringframework/user/domain/DaoFactory.java
@@ -3,8 +3,21 @@ package io.ggammu.study.tobyspringframework.user.domain;
 // Factory Class
 public class DaoFactory {
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new UserDao(connectionMaker());
         return userDao;
+    }
+
+    public AccountDao accountDao() {
+        AccountDao accountDao = new AccountDao(connectionMaker());
+        return accountDao;
+    }
+
+    public MessageDao messageDao() {
+        MessageDao messageDao = new MessageDao(connectionMaker());
+        return messageDao;
+    }
+
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 }
