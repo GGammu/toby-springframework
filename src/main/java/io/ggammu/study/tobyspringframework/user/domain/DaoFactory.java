6377e50cf33a2dbbd8fdd829f42f5f730e626fb4
Young Hwang <ykcul02@gmail.com>
2021-04-30 23:52:43 +0900

    Spring Framework 사용
    Annotation을 이용한 Spring Framework 사용

--- src/main/java/io/ggammu/study/tobyspringframework/user/domain/DaoFactory.java
+++ src/main/java/io/ggammu/study/tobyspringframework/user/domain/DaoFactory.java
@@ -1,22 +1,30 @@
 package io.ggammu.study.tobyspringframework.user.domain;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 // Factory Class
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
+    @Bean
     public AccountDao accountDao() {
         AccountDao accountDao = new AccountDao(connectionMaker());
         return accountDao;
     }
 
+    @Bean
     public MessageDao messageDao() {
         MessageDao messageDao = new MessageDao(connectionMaker());
         return messageDao;
     }
 
+    @Bean
     public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
--- src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
+++ src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
         UserDao dao = new DaoFactory().userDao();
 
         User user = new User();
