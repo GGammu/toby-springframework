c4827fd727f0851bbe1be4a20a5107397f5f3194
Young Hwang <ykcul02@gmail.com>
2021-05-05 22:46:39 +0900

    Junit 적용
    Junit 테스트 적용

--- src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
+++ src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
@@ -1,15 +1,48 @@
 package io.ggammu.study.tobyspringframework.user.domain;
 
+import org.junit.jupiter.api.Test;
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryListener;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+import org.junit.platform.launcher.listeners.TestExecutionSummary;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import java.io.PrintWriter;
 import java.sql.SQLException;
 
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
 public class UserDaoTest {
+    SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDaoTest runner = new UserDaoTest();
+        runner.runOne();
+        TestExecutionSummary summary = runner.listener.getSummary();
+        summary.printTo(new PrintWriter(System.out));
+    }
+
+    private void runOne() {
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selectClass(UserDaoTest.class))
+                .build();
+        Launcher launcher = LauncherFactory.create();
+        TestPlan testPlan = launcher.discover(request);
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+    }
+
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
