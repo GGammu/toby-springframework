2a751a9cbc333eb5d6b9c5f5c6fd0281f816b11a
Young Hwang <ykcul02@gmail.com>
2021-05-09 01:02:26 +0900

    Context 주입 받기
    Context 주입 받기

--- src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
+++ src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
@@ -2,6 +2,7 @@ package io.ggammu.study.tobyspringframework.user.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.TestPlan;
@@ -9,9 +10,11 @@ import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.PrintWriter;
 import java.sql.SQLException;
@@ -20,9 +23,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { DaoFactory.class })
+//@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
     private SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+    @Autowired
+    private ApplicationContext context;
     private UserDao userDao;
+    private User user1;
+    private User user2;
+    private User user3;
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDaoTest runner = new UserDaoTest();
@@ -44,17 +56,18 @@ public class UserDaoTest {
     @BeforeEach
     public void setUp() {
 //        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         userDao = context.getBean("userDao", UserDao.class);
+
+        user1 = new User("1", "user1", "password1");
+        user2 = new User("2", "user2", "password2");
+        user3 = new User("3", "user3", "password3");
     }
 
     @Test
     public void addAndGet() throws SQLException, ClassNotFoundException {
         userDao.deleteAll();
         assertThat(userDao.getCount()).isEqualTo(0);
-
-        User user1 = new User("1", "user1", "password1");
-        User user2 = new User("2", "user2", "password2");
 
         userDao.deleteAll();
 
@@ -74,11 +87,6 @@ public class UserDaoTest {
 
     @Test
     public void count() throws SQLException, ClassNotFoundException {
-        // given
-        User user1 = new User("1", "user1", "password1");
-        User user2 = new User("2", "user2", "password2");
-        User user3 = new User("3", "user3", "password3");
-
         // then
         userDao.deleteAll();
         assertThat(userDao.getCount()).isEqualTo(0);
