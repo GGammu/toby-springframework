01bce8a81c4f1a05c33ed62cb28b4c49729298c8
Young Hwang <ykcul02@gmail.com>
2021-05-08 17:19:44 +0900

    Test 코드 리팩토링
    Test 코드 리팩토링

--- src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
+++ src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserDaoTest.java
@@ -1,5 +1,6 @@
 package io.ggammu.study.tobyspringframework.user.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
@@ -9,7 +10,6 @@ import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 
@@ -21,7 +21,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 public class UserDaoTest {
-    SummaryGeneratingListener listener = new SummaryGeneratingListener();
+    private SummaryGeneratingListener listener = new SummaryGeneratingListener();
+    private UserDao userDao;
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDaoTest runner = new UserDaoTest();
@@ -40,42 +41,45 @@ public class UserDaoTest {
         launcher.execute(request);
     }
 
-    @Test
-    public void addAndGet() throws SQLException, ClassNotFoundException {
+    @BeforeEach
+    public void setUp() {
 //        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
+        userDao = context.getBean("userDao", UserDao.class);
+    }
 
-        dao.deleteAll();
-        assertThat(dao.getCount()).isEqualTo(0);
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
 
         User user1 = new User("1", "user1", "password1");
         User user2 = new User("2", "user2", "password2");
 
-        dao.deleteAll();
+        userDao.deleteAll();
 
-        dao.add(user1);
-        dao.add(user2);
-        assertThat(dao.getCount()).isEqualTo(2);
+        // then
+        userDao.add(user1);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
 
-        User userGet1 = dao.get(user1.getId());
+        User userGet1 = userDao.get(user1.getId());
         assertThat(userGet1.getName()).isEqualTo(user1.getName());
         assertThat(userGet1.getPassword()).isEqualTo(user1.getPassword());
 
-        User userGet2 = dao.get(user2.getId());
+        User userGet2 = userDao.get(user2.getId());
         assertThat(userGet2.getName()).isEqualTo(user2.getName());
         assertThat(userGet2.getPassword()).isEqualTo(user2.getPassword());
     }
 
     @Test
     public void count() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-
+        // given
         User user1 = new User("1", "user1", "password1");
         User user2 = new User("2", "user2", "password2");
         User user3 = new User("3", "user3", "password3");
 
+        // then
         userDao.deleteAll();
         assertThat(userDao.getCount()).isEqualTo(0);
 
@@ -92,13 +96,14 @@ public class UserDaoTest {
     @Test
     public void getUserFailure() throws SQLException, ClassNotFoundException {
         // given
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = context.getBean("userDao", UserDao.class);
-
         userDao.deleteAll();
+
         assertThat(userDao.getCount()).isEqualTo(0);
 
+        // when
         Throwable thrown = catchThrowable(() -> userDao.get("unknown"));
+
+        // then
         assertThat(thrown).isInstanceOf(EmptyResultDataAccessException.class);
     }
 }
