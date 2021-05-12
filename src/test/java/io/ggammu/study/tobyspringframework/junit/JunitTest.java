c69fa0145b1c6d45a89f8300860012b5b7f6fd60
Young Hwang <ykcul02@gmail.com>
2021-05-12 23:17:33 +0900

    test(JunitTest): Add context valriable for test

--- src/test/java/io/ggammu/study/tobyspringframework/junit/JunitTest.java
+++ src/test/java/io/ggammu/study/tobyspringframework/junit/JunitTest.java
@@ -1,31 +1,49 @@
 package io.ggammu.study.tobyspringframework.junit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "/junit.xml")
 public class JunitTest {
 //    static JunitTest testObject;
+    @Autowired
+    ApplicationContext context;
     static Set<JunitTest> testObjects = new HashSet<JunitTest>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() {
         assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 
     @Test
     public void test2() {
         assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+        System.out.println("value test " + this.context);
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
         assertThat(testObjects).doesNotContain(this);
         testObjects.add(this);
+        System.out.println("value test " + this.context);
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 }
