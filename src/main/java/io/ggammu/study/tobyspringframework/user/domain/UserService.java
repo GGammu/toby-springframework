1a775c6b203aae01d74e046714b24793865e36f9
Young Hwang <ykcul02@gmail.com>
2021-06-03 22:17:56 +0900

    refactor: 사용자 레벨 업그레이드 수정

--- src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserService.java
+++ src/main/java/io/ggammu/study/tobyspringframework/user/domain/UserService.java
@@ -32,6 +32,30 @@ public class UserService {
             if (changed) {
                 userDao.update(user);
             }
+
+            if (canUpgradeLevel(user)) {
+                upgradeLevel(user);
+            }
+        }
+    }
+
+    private void upgradeLevel(User user) {
+        if (user.getLevel() == Level.BASIC) user.setLevel(Level.SILVER);
+        else if (user.getLevel() == Level.SILVER) user.setLevel(Level.GOLD);
+        userDao.update(user);
+    }
+
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch (currentLevel) {
+            case BASIC:
+                return user.getLogin() >= 50;
+            case SILVER:
+                return user.getRecommend() >= 30;
+            case GOLD:
+                return false;
+            default:
+                throw new IllegalArgumentException("Unknown Level: " + currentLevel);
         }
     }
 
