7702040eda375dcace6c43cf4e87b71bd3943180
Young Hwang <ykcul02@gmail.com>
2021-06-18 22:17:34 +0900

    fix: UserServiceTest 실행 오류 @DirtiesContext 추가

--- src/test/java/io/ggammu/study/tobyspringframework/user/domain/UserServiceTest.java
+++ src/test/java/io/ggammu/study/tobyspringframework/user/domain/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @ExtendWith(SpringExtension.class)
+@DirtiesContext
 @ContextConfiguration(classes = { TestDaoFactory.class })
 class UserServiceTest {
 
