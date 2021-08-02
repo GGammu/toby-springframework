6c9da422cc06f65bbc9f7f491604152a18757e42
Young Hwang <ykcul02@gmail.com>
2021-08-02 21:02:07 +0900

    feat: proxy class 만들기

---
+++ src/main/java/io/ggammu/study/tobyspringframework/jdk/Hello.java
@@ -0,0 +1,7 @@
+package io.ggammu.study.tobyspringframework.jdk;
+
+public interface Hello {
+    String sayHello(String name);
+    String sayHi(String name);
+    String sayThankYou(String name);
+}
