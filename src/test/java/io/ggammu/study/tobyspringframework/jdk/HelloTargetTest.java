a96575af13fba0be9009dace505a23f8a0042d14
Young Hwang <ykcul02@gmail.com>
2021-08-05 23:00:57 +0900

    fix: proxy test 수정

--- src/test/java/io/ggammu/study/tobyspringframework/jdk/HelloTargetTest.java
+++ src/test/java/io/ggammu/study/tobyspringframework/jdk/HelloTargetTest.java
@@ -9,6 +9,8 @@ class HelloTargetTest {
     public void simpleProxy() {
         Hello hello = new HelloTarget();
         assertThat(hello.sayHello("Toby")).isEqualTo("Hello Toby");
+        assertThat(hello.sayHi("Toby")).isEqualTo("Hi Toby");
+        assertThat(hello.sayThankYou("Toby")).isEqualTo("Thank You Toby");
     }
 
 }
