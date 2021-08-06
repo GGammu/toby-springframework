7835591f8a2053900b25efaebff51fcbb7531b68
Young Hwang <ykcul02@gmail.com>
2021-08-06 23:15:30 +0900

    feat: Hello Proxy class 생성

---
+++ src/main/java/io/ggammu/study/tobyspringframework/jdk/HelloUppercase.java
@@ -0,0 +1,20 @@
+package io.ggammu.study.tobyspringframework.jdk;
+
+public class HelloUppercase implements Hello {
+    Hello hello;
+
+    @Override
+    public String sayHello(String name) {
+        return null;
+    }
+
+    @Override
+    public String sayHi(String name) {
+        return null;
+    }
+
+    @Override
+    public String sayThankYou(String name) {
+        return null;
+    }
+}
