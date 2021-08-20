fad561ecffed36dd54b07f675472c0ba9c149205
Young Hwang <ykcul02@gmail.com>
2021-08-20 23:01:51 +0900

    feat: factoryBean Test 추가

---
+++ src/test/java/io/ggammu/study/tobyspringframework/factorybean/FactoryBeanTest.java
@@ -0,0 +1,25 @@
+package io.ggammu.study.tobyspringframework.factorybean;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+class FactoryBeanTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    public void getMessageFromFactoryBean() {
+        Object message = context.getBean("message");
+        assertThat(message).isEqualTo(Message.class);
+        assertThat(((Message)message).getText()).isEqualTo("Factory Bean");
+    }
+}
