77d233f6ba48d2b7b07de4f63f2e1e1554429720
Young Hwang <ykcul02@gmail.com>
2021-08-31 21:43:44 +0900

    feat: ProxyFactoryBean 사용 하기

--- src/test/java/io/ggammu/study/tobyspringframework/jdk/proxy/DynamicProxyTest.java
+++ src/test/java/io/ggammu/study/tobyspringframework/jdk/proxy/DynamicProxyTest.java
@@ -1,10 +1,29 @@
 package io.ggammu.study.tobyspringframework.jdk.proxy;
 
+import com.zaxxer.hikari.pool.ProxyFactory;
+import io.ggammu.study.tobyspringframework.jdk.Hello;
+import io.ggammu.study.tobyspringframework.jdk.HelloTarget;
+import io.ggammu.study.tobyspringframework.jdk.UppercaseHandler;
+import java.lang.reflect.Proxy;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 public class DynamicProxyTest {
     @Test
     public void simpleProxy() {
+        Hello proxiedHello = (Hello) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[] { Hello.class },
+                new UppercaseHandler(new HelloTarget())
+        );
+    }
 
+    @Test
+    public void proxyFactoryBean() {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(new HelloTarget());
+        proxyFactoryBean.addAdvice(new UppercaseAdvice());
+
+        Hello proxiedHello = (Hello) proxyFactoryBean.getObject();
     }
 }
