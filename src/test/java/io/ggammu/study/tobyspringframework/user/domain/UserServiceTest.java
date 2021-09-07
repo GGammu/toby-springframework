c5a827f8c7f69e50a2656d3fa27f6470e7d6dd85
Young Hwang <ykcul02@gmail.com>
2021-09-07 22:07:35 +0900

    fix: Test ProxyFactoryBean 변경

--- src/test/java/io/ggammu/study/tobyspringframework/user/domain/UserServiceTest.java
+++ src/test/java/io/ggammu/study/tobyspringframework/user/domain/UserServiceTest.java
@@ -19,6 +19,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.mail.MailSender;
@@ -179,7 +180,7 @@ class UserServiceTest {
         testUserService.setUserDao(userDao);
         testUserService.setMailSender(mailSender);
 
-        TxProxyFactoryBean txProxyFactoryBean = context.getBean("&userService", TxProxyFactoryBean.class);
+        ProxyFactoryBean txProxyFactoryBean = context.getBean("&userService", ProxyFactoryBean.class);
         txProxyFactoryBean.setTarget(testUserService);
         UserService txUserService = (UserService) txProxyFactoryBean.getObject();
 
