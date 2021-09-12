2d794e19492d43d44aeac63c409621e64ee0a470
Young Hwang <ykcul02@gmail.com>
2021-09-12 22:12:32 +0900

    feat: 테스트용 UserService 구현 클래스

---
+++ src/test/java/io/ggammu/study/tobyspringframework/service/user/TestUserServiceImpl.java
@@ -0,0 +1,15 @@
+package io.ggammu.study.tobyspringframework.service.user;
+
+import io.ggammu.study.tobyspringframework.user.domain.TestUserServiceException;
+import io.ggammu.study.tobyspringframework.user.domain.User;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestUserServiceImpl extends UserServiceImpl {
+    private String id = "madnite1";
+
+    protected void upgradeLevel(User user) {
+        if (user.getId().equals(this.id)) throw new TestUserServiceException();
+        super.upgradeLevel(user);
+    }
+
+}
