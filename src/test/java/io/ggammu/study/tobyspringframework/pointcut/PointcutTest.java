2c605c51d0fcba6dae1259d3bfc36f44dbe0c758
Young Hwang <ykcul02@gmail.com>
2021-09-25 18:00:56 +0900

    fix: 포인트컷 표현식을 이용한 포인트컷 적용

--- src/test/java/io/ggammu/study/tobyspringframework/pointcut/PointcutTest.java
+++ src/test/java/io/ggammu/study/tobyspringframework/pointcut/PointcutTest.java
@@ -3,6 +3,7 @@ package io.ggammu.study.tobyspringframework.pointcut;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.data.spel.ExpressionDependencies;
 
 class PointcutTest {
     @Test
@@ -29,5 +30,27 @@ class PointcutTest {
                 pointcut.getMethodMatcher().matches(
                         Target.class.getMethod("method"), null)
         ).isEqualTo(false);
+    }
+
+    @Test
+    public void pointcut() throws NoSuchMethodException {
+        targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+    }
+
+    private void targetClassPointcutMatches(String expression, boolean... expected) throws NoSuchMethodException {
+        pointcutMatches(expression, expected[0], Target.class, "hello");
+        pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+        pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+        pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+        pointcutMatches(expression, expected[4], Target.class, "method");
+        pointcutMatches(expression, expected[5], Bean.class, "method");
+    }
+
+    private void pointcutMatches(String expression, boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws NoSuchMethodException {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(expression);
+
+        assertThat(pointcut.getClassFilter().matches(clazz) &&
+                pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null)).isEqualTo(expected);
     }
 }
