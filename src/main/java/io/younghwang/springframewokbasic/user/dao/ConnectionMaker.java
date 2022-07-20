390eb2638570c0597c86429f3bf71433fb1fe826
Young Hwang <ykcul02@gmail.com>
2022-07-20 19:51:41 +0900

    01.03.02 인터페이스의 도입

---
+++ src/main/java/io/younghwang/springframewokbasic/user/dao/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package io.younghwang.springframewokbasic.user.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    public Connection makeConnection() throws ClassNotFoundException, SQLException;
+}
