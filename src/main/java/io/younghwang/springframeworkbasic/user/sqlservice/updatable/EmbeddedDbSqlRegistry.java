ba1e221a7d74f3b08502f6ae229fc6441c1c3925
Young Hwang <ykcul02@gmail.com>
2022-12-02 00:51:04 +0900

    07.05.03 트랜잭션 적용

--- src/main/java/io/younghwang/springframeworkbasic/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ src/main/java/io/younghwang/springframeworkbasic/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -6,12 +6,24 @@ import io.younghwang.springframeworkbasic.user.exception.SqlUpdateFailureExcepti
 import io.younghwang.springframeworkbasic.user.sqlservice.UpdatableSqlRegistry;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.sql.DataSource;
 import java.util.Map;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
+    private DataSource dataSource;
     private JdbcTemplate jdbc;
+    private TransactionTemplate transactionTemplate;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        transactionTemplate = new TransactionTemplate(new DataSourceTransactionManager(dataSource));
+    }
 
     public void setJdbc(DataSource dataSource) {
         this.jdbc = new JdbcTemplate(dataSource);
@@ -41,8 +53,13 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 
     @Override
     public void updateSql(Map<String, String> sqlmap) throws SqlUpdateFailureException {
-        for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
-            updateSql(entry.getKey(), entry.getValue());
-        }
+        transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+            @Override
+            protected void doInTransactionWithoutResult(TransactionStatus status) {
+                for (Map.Entry<String, String> entry : sqlmap.entrySet()) {
+                    updateSql(entry.getKey(), entry.getValue());
+                }
+            }
+        });
     }
 }
--- src/test/java/io/younghwang/springframeworkbasic/user/sqlservice/EmbeddedDbSqlRegistryTest.java
+++ src/test/java/io/younghwang/springframeworkbasic/user/sqlservice/EmbeddedDbSqlRegistryTest.java
@@ -1,10 +1,14 @@
 package io.younghwang.springframeworkbasic.user.sqlservice;
 
+import io.younghwang.springframeworkbasic.user.exception.SqlUpdateFailureException;
 import io.younghwang.springframeworkbasic.user.sqlservice.updatable.EmbeddedDbSqlRegistry;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
@@ -18,8 +22,25 @@ public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest 
                 .addScript("classpath:/io/younghwang/springframeworkbasic/embeddeddb/schema.sql")
                 .build();
         EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
+        embeddedDbSqlRegistry.setDataSource(db);
         embeddedDbSqlRegistry.setJdbc(db);
         return embeddedDbSqlRegistry;
+    }
+
+    @Test
+    void transactionalUpgrade() {
+        // given
+        checkFindResult("SQL1", "SQL2", "SQL3");
+        // when
+        Map<String, String> sqlmap = new HashMap<String, String>();
+        sqlmap.put("KEY1", "Modified1");
+        sqlmap.put("KEY999", "Modified999");
+        // then
+        try {
+            sqlRegistry.updateSql(sqlmap);
+        } catch (SqlUpdateFailureException e) {
+            checkFindResult("SQL1", "SQL2", "SQL3");
+        }
     }
 
     @AfterEach
